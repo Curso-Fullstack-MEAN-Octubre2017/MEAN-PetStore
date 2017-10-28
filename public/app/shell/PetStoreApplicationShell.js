7160e6b07405379008e6e77acd059f014a0974b7
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2017-10-28 22:02:52 +0200

    Ejemplo: Como prevenir abandonar un formulario con datos sin guardar

--- public/app/shell/PetStoreApplicationShell.js
+++ public/app/shell/PetStoreApplicationShell.js
@@ -13,5 +13,18 @@ angular.module('petStoreApplicationShell')
             	$rootScope.userPreferences = response;
             });
        
+            $scope.$on('$locationChangeStart', function(event, current, next) {
+            	console.log("$locationChangeStart", event, current, next);
+            	/* 
+            	 * AngularJS sets the CSS classes ng-pristine and ng-dirty on any input field you've used ng-model on, 
+            	 * and your FormController has the properties $pristine and $dirty 
+            	 * which you can check to see if the form is dirty or not
+            	 */
+            	if($(".ng-dirty").length > 0) {
+            		if(!confirm("Hay datos sin guardar. ¿Quires continuar y perder dichos cambios?")){
+            			event.preventDefault();
+            		}
+            	}
+            });
         }
     });
